4ecc7c6c33388a42fc9ae5e34c0b175c650ff805
Stephen Kitomary <kitomarystephen@gmail.com>
2025-04-06 18:53:56 -0400

    newa

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4zyAb1zxRbvYVxn3j7AhmNfMdbUHKBT2zO73vD5kVVg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
